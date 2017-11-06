0495eaeee5e59df2f5b0d0f4a2bf2014ae5c0b91
Stas Kirichok <kirichok.stas@gmail.com>
2017-11-06 19:48:11 +0200

    [Add] Added loop recognizing

--- algorithmTester/Algorithm.swift
+++ algorithmTester/Algorithm.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-enum AtomicCommand: String {
+enum AtomicCommand: String, CustomDebugStringConvertible {
   case moveForward = ">"
   case moveBackward = "<"
   case increaseValue = "+"
@@ -17,18 +17,37 @@ enum AtomicCommand: String {
   case writeValue = "."
   case startLoop = "["
   case endLoop = "]"
+  case nullify = "n"
+
+  var debugDescription: String {
+    return rawValue
+  }
 }
 
-struct MetaCommand {
-  let command: AtomicCommand
-  let repeatTime: Int
+enum MetaCommand: CustomDebugStringConvertible {
+  case fusing(command: AtomicCommand, repeatTime: Int)
+  indirect case loop(sequence: [MetaCommand])
+
+  var debugDescription: String {
+    switch self {
+    case .fusing(let command, let repeatTime):
+      return "\(command.debugDescription)\(repeatTime)"
+    case .loop(let sequence):
+      return sequence.reduce("[", { (result, metaCommand) -> String in
+        return "\(result) \(metaCommand.debugDescription)"
+      }) + "]"
+    }
+  }
 }
 
 func parse(code: String) -> [AtomicCommand] {
-  return code.characters.map { AtomicCommand(rawValue: String($0))! }
+  let regexp = try! NSRegularExpression(pattern: "\\[(-|\\+)\\]", options: [])
+  let range = NSMakeRange(0, code.characters.count)
+  let updatedCode = regexp.stringByReplacingMatches(in: code, options: [], range: range, withTemplate: "n")
+  return updatedCode.characters.map { AtomicCommand(rawValue: String($0))! }
 }
 
-func optimize(sequence: [AtomicCommand]) -> [MetaCommand] {
+func fusing(sequence: [AtomicCommand]) -> [MetaCommand] {
   var processingCommand: AtomicCommand!
   var processingCommandCount = 1
   var result = [MetaCommand]()
@@ -38,14 +57,14 @@ func optimize(sequence: [AtomicCommand]) -> [MetaCommand] {
     } else if command == processingCommand && command != .endLoop && command != .startLoop {
       processingCommandCount += 1
     } else {
-      let metaCommand = MetaCommand(command: processingCommand, repeatTime: processingCommandCount)
+      let metaCommand = MetaCommand.fusing(command: processingCommand, repeatTime: processingCommandCount)
       result.append(metaCommand)
       processingCommand = command
       processingCommandCount = 1
     }
 
     if index == sequence.count - 1 {
-      let metaCommand = MetaCommand(command: processingCommand, repeatTime: processingCommandCount)
+      let metaCommand = MetaCommand.fusing(command: processingCommand, repeatTime: processingCommandCount)
       result.append(metaCommand)
     }
   }
@@ -53,21 +72,47 @@ func optimize(sequence: [AtomicCommand]) -> [MetaCommand] {
   return result
 }
 
+func wrappingLoops(in sequence: [MetaCommand]) -> [MetaCommand] {
+  var loops: [Int: [MetaCommand]] = [:]
+  var nestingLevel = 0
+  loops[nestingLevel] = [MetaCommand]()
+  for metaCommand in sequence {
+    if case .fusing(let command, _) = metaCommand {
+      if command == .startLoop {
+        nestingLevel += 1
+        loops[nestingLevel] = [MetaCommand]()
+      } else if command == .endLoop {
+        let currentLoop = loops[nestingLevel]!
+        nestingLevel -= 1
+        var outerLoop = loops[nestingLevel]!
+        outerLoop.append(.loop(sequence: currentLoop))
+        loops[nestingLevel] = outerLoop
+      } else {
+        var currentLoop = loops[nestingLevel]!
+        currentLoop.append(metaCommand)
+        loops[nestingLevel] = currentLoop
+      }
+    }
+  }
+
+  return loops[0]!
+}
+
 var memoryCount = 1
 var memoryPointer = UnsafeMutablePointer<UInt8>.allocate(capacity: memoryCount)
 var currentMemoryPosition = 0
 var outputSequence: [UnicodeScalar] = []
 var inputSequence: [UInt8]!
-var loopPointers = [Int]()
-var commandPointer = 0
+var lastPointer: UnsafeMutablePointer<UInt8>!
 
 func brainLuck(_ code: String, input: String) -> String {
   cleanUp()
 
   inputSequence = input.characters.map { UInt8(String($0).unicodeScalars.first!.value) }.reversed()
   let commandSequence = parse(code: code)
-  let optimizedSequence = optimize(sequence: commandSequence)
-  execute(sequence: optimizedSequence)
+  let optimizedSequence = fusing(sequence: commandSequence)
+  let loopRichSequence = wrappingLoops(in: optimizedSequence)
+  execute(sequence: loopRichSequence)
   return outputSequence.reduce("") { (result, current) -> String in
     return "\(result)\(current.escaped(asASCII: true))"
   }
@@ -80,63 +125,60 @@ func cleanUp() {
   memoryPointer.deallocate(capacity: memoryCount)
   memoryPointer = UnsafeMutablePointer<UInt8>.allocate(capacity: memoryCount)
   outputSequence.removeAll()
-  loopPointers.removeAll()
-  commandPointer = 0
+  lastPointer = UnsafeMutablePointer<UInt8>(memoryPointer)
 }
 
 func execute(sequence: [MetaCommand]) {
-  while (commandPointer < sequence.count) {
-    let command = sequence[commandPointer]
-    if execute(command: command) {
-      commandPointer += 1
-    }
+  sequence.forEach { metaCommand in
+    execute(metaCommand: metaCommand)
   }
 }
 
-func execute(command: MetaCommand) -> Bool {
-  switch command.command {
-  case .moveForward:
-    currentMemoryPosition += command.repeatTime
-    if currentMemoryPosition >= memoryCount {
-      memoryCount = currentMemoryPosition + 1
+func execute(metaCommand: MetaCommand) {
+  switch metaCommand {
+  case .fusing(let command, let repeatTime):
+    switch command {
+    case .moveForward:
+      currentMemoryPosition += repeatTime
+      if currentMemoryPosition >= memoryCount {
+        memoryCount = currentMemoryPosition + 1
+      }
+      lastPointer = lastPointer.advanced(by: repeatTime)
+
+    case .moveBackward:
+      currentMemoryPosition -= repeatTime
+      lastPointer = lastPointer.advanced(by: -repeatTime)
+
+    case .increaseValue:
+      let value = lastPointer.pointee
+      lastPointer.pointee = value &+ UInt8(repeatTime)
+
+    case .decreaseValue:
+      let value = lastPointer.pointee
+      lastPointer.pointee = value &- UInt8(repeatTime)
+
+    case .readValue:
+      for _ in 0..<repeatTime {
+        let inputScalar = inputSequence.popLast()!
+        lastPointer.pointee = inputScalar
+      }
+
+    case .writeValue:
+      for _ in 0..<repeatTime {
+        outputSequence.append(UnicodeScalar(lastPointer.pointee))
+      }
+
+    case .nullify:
+      lastPointer.pointee = 0
+
+    default:
+      fatalError("incorrect command")
     }
-
-  case .moveBackward:
-    currentMemoryPosition -= command.repeatTime
-
-  case .increaseValue:
-    let value = memoryPointer[currentMemoryPosition]
-    memoryPointer[currentMemoryPosition] = value &+ UInt8(command.repeatTime)
-
-  case .decreaseValue:
-    let value = memoryPointer[currentMemoryPosition]
-    memoryPointer[currentMemoryPosition] = value &- UInt8(command.repeatTime)
-
-  case .readValue:
-    for _ in 0..<command.repeatTime {
-      let inputScalar = inputSequence.popLast()!
-      memoryPointer[currentMemoryPosition] = inputScalar
-    }
-
-  case .writeValue:
-    for _ in 0..<command.repeatTime {
-      outputSequence.append(UnicodeScalar(memoryPointer[currentMemoryPosition]))
-    }
-
-  case .startLoop:
-    if memoryPointer[currentMemoryPosition] != 0 {
-      loopPointers.append(commandPointer)
-    }
-
-  case .endLoop:
-    let loopPointer = loopPointers.popLast()!
-    if memoryPointer[currentMemoryPosition] != 0 {
-      commandPointer = loopPointer
-      return false
+  case .loop(let sequence):
+    while lastPointer.pointee != 0 {
+      execute(sequence: sequence)
     }
   }
-  
-  return true
 }
 
 
