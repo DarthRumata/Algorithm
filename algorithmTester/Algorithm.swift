bb0876f3d851db4895bd06066781a3b56740d55f
Stas Kirichok <kirichok.stas@gmail.com>
2017-11-06 15:09:54 +0200

    [Add] Added optimization of command sequence: collapsed equal commands

--- algorithmTester/Algorithm.swift
+++ algorithmTester/Algorithm.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-enum Command: String {
+enum AtomicCommand: String {
   case moveForward = ">"
   case moveBackward = "<"
   case increaseValue = "+"
@@ -19,8 +19,38 @@ enum Command: String {
   case endLoop = "]"
 }
 
-func parse(code: String) -> [Command] {
-  return code.characters.map { Command(rawValue: String($0))! }
+struct MetaCommand {
+  let command: AtomicCommand
+  let repeatTime: Int
+}
+
+func parse(code: String) -> [AtomicCommand] {
+  return code.characters.map { AtomicCommand(rawValue: String($0))! }
+}
+
+func optimize(sequence: [AtomicCommand]) -> [MetaCommand] {
+  var processingCommand: AtomicCommand!
+  var processingCommandCount = 1
+  var result = [MetaCommand]()
+  for (index, command) in sequence.enumerated() {
+    if processingCommand == nil {
+      processingCommand = command
+    } else if command == processingCommand && command != .endLoop && command != .startLoop {
+      processingCommandCount += 1
+    } else {
+      let metaCommand = MetaCommand(command: processingCommand, repeatTime: processingCommandCount)
+      result.append(metaCommand)
+      processingCommand = command
+      processingCommandCount = 1
+    }
+
+    if index == sequence.count - 1 {
+      let metaCommand = MetaCommand(command: processingCommand, repeatTime: processingCommandCount)
+      result.append(metaCommand)
+    }
+  }
+
+  return result
 }
 
 var memoryCount = 1
@@ -32,13 +62,12 @@ var loopPointers = [Int]()
 var commandPointer = 0
 
 func brainLuck(_ code: String, input: String) -> String {
-  defer {
-    cleanUp()
-  }
+  cleanUp()
 
   inputSequence = input.characters.map { UInt8(String($0).unicodeScalars.first!.value) }.reversed()
   let commandSequence = parse(code: code)
-  execute(sequence: commandSequence)
+  let optimizedSequence = optimize(sequence: commandSequence)
+  execute(sequence: optimizedSequence)
   return outputSequence.reduce("") { (result, current) -> String in
     return "\(result)\(current.escaped(asASCII: true))"
   }
@@ -48,12 +77,14 @@ func cleanUp() {
   currentMemoryPosition = 0
   memoryPointer.initialize(to: 0, count: memoryCount)
   memoryCount = 1
+  memoryPointer.deallocate(capacity: memoryCount)
+  memoryPointer = UnsafeMutablePointer<UInt8>.allocate(capacity: memoryCount)
   outputSequence.removeAll()
   loopPointers.removeAll()
   commandPointer = 0
 }
 
-func execute(sequence: [Command]) {
+func execute(sequence: [MetaCommand]) {
   while (commandPointer < sequence.count) {
     let command = sequence[commandPointer]
     if execute(command: command) {
@@ -62,31 +93,35 @@ func execute(sequence: [Command]) {
   }
 }
 
-func execute(command: Command) -> Bool {
-  switch command {
+func execute(command: MetaCommand) -> Bool {
+  switch command.command {
   case .moveForward:
-    currentMemoryPosition += 1
+    currentMemoryPosition += command.repeatTime
     if currentMemoryPosition >= memoryCount {
       memoryCount = currentMemoryPosition + 1
     }
 
   case .moveBackward:
-    currentMemoryPosition -= 1
+    currentMemoryPosition -= command.repeatTime
 
   case .increaseValue:
     let value = memoryPointer[currentMemoryPosition]
-    memoryPointer[currentMemoryPosition] = value &+ 1
+    memoryPointer[currentMemoryPosition] = value &+ UInt8(command.repeatTime)
 
   case .decreaseValue:
     let value = memoryPointer[currentMemoryPosition]
-    memoryPointer[currentMemoryPosition] = value &- 1
+    memoryPointer[currentMemoryPosition] = value &- UInt8(command.repeatTime)
 
   case .readValue:
-    let inputScalar = inputSequence.popLast()!
-    memoryPointer[currentMemoryPosition] = inputScalar
+    for _ in 0..<command.repeatTime {
+      let inputScalar = inputSequence.popLast()!
+      memoryPointer[currentMemoryPosition] = inputScalar
+    }
 
   case .writeValue:
-    outputSequence.append(UnicodeScalar(memoryPointer[currentMemoryPosition]))
+    for _ in 0..<command.repeatTime {
+      outputSequence.append(UnicodeScalar(memoryPointer[currentMemoryPosition]))
+    }
 
   case .startLoop:
     if memoryPointer[currentMemoryPosition] != 0 {
